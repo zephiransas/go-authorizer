2db52b7dce9580c30c0d2776f804ab2704c50521
Takafumi Yoshida <yoshida.takafumi@classmethod.jp>
2020-12-30 20:12:18 +0900

    First Commit

---
+++ authorizer/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+	"strings"
+)
+
+func handleRequest(_ context.Context, event events.APIGatewayCustomAuthorizerRequest) (events.APIGatewayCustomAuthorizerResponse, error) {
+	log.Println("Method ARN: " + event.MethodArn)
+	log.Println("token:" + event.AuthorizationToken)
+
+	tmp := strings.Split(event.MethodArn, ":")
+	region := tmp[3]
+	awsAccountID := tmp[4]
+	apiGatewayArnTmp := strings.Split(tmp[5], "/")
+
+	res := NewAuthorizeResponse("*", awsAccountID)
+
+	res.Region = region
+	res.APIID = apiGatewayArnTmp[0]
+	res.Stage = apiGatewayArnTmp[1]
+
+	if event.AuthorizationToken == "allow" {
+		res.addMethod(Allow, apiGatewayArnTmp[2], "*")
+	} else {
+		res.addMethod(Deny, apiGatewayArnTmp[2], "*")
+	}
+	return res.APIGatewayCustomAuthorizerResponse, nil
+}
+
+func main()  {
+	lambda.Start(handleRequest)
+}
+
+type Effect int
+
+const (
+	Allow Effect = iota
+	Deny
+)
+
+func (e Effect) String() string {
+	switch e {
+		case Allow: return "Allow"
+		case Deny: return "Deny"
+	}
+	return ""
+}
+
+type AuthorizeResponse struct {
+	events.APIGatewayCustomAuthorizerResponse
+	Region		string
+	AccountID 	string
+	APIID 		string
+	Stage 		string
+}
+
+func NewAuthorizeResponse(principalID string, accountID string) *AuthorizeResponse {
+	return &AuthorizeResponse{
+		APIGatewayCustomAuthorizerResponse: events.APIGatewayCustomAuthorizerResponse{
+			PrincipalID:        principalID,
+			PolicyDocument:     events.APIGatewayCustomAuthorizerPolicy{
+				Version: "2012-10-17",
+			},
+		},
+		Region: "*",
+		AccountID: accountID,
+		APIID: "*",
+		Stage: "*",
+	}
+}
+
+func (r *AuthorizeResponse)addMethod(effect Effect, verb string, resource string) {
+	resourceArn := "arn:aws:execute-api:" +
+		r.Region + ":" +
+		r.AccountID + ":" +
+		r.APIID + "/" +
+		r.Stage + "/" +
+		verb + "/" +
+		strings.TrimLeft(resource, "/")
+
+	s := events.IAMPolicyStatement{
+		Effect:   effect.String(),
+		Action:   []string{"execute-api:Invoke"},
+		Resource: []string{resourceArn},
+	}
+
+	r.PolicyDocument.Statement = append(r.PolicyDocument.Statement, s)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/zephiransas/go-authorizer
+
+go 1.14
+
+require github.com/aws/aws-lambda-go v1.6.0
